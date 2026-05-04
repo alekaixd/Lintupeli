f4a46228f52733dd493bb6fa2ab724ba68d38888
UnnaHaitto <125347964+UnnaHaitto@users.noreply.github.com>
2026-05-04 12:58:53 +0300

    changes to API

--- login.js
+++ login.js
@@ -2,7 +2,7 @@ function login() {
         let username = document.getElementById("username").value;
         let password = document.getElementById("password").value;
 
-        fetch("http://127.0.0.1:3000/login", {
+        fetch("/login", {
             method: "POST",
             headers: {
                 "Content-Type": "application/json"
