ae58494deecacf8229884a4823df7d175551aac7
Alex Sviridenko <150971393+AlexDenkko@users.noreply.github.com>
2026-05-07 13:33:36 +0300

    added functinality that when i click outside infobox it closes it if its open

--- game.js
+++ game.js
@@ -271,6 +271,11 @@ infoBtn.addEventListener("click", function infoEvent() {
 	openInfo(iTitle, iText);
 });
 closeInfoBtn.addEventListener("click", closeInfo);
+infoOverlay.addEventListener("click", function (event) {
+	if (event.target === infoOverlay) {
+		closeInfo();
+	}
+});
 
 async function logout() {
 	const response = await fetch("http://localhost:3000/logout", {
